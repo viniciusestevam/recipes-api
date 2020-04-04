edbe07207bcfb098066e427edb9c742a20c7140a
estevam31 <estevamvinicius31@gmail.com>
2020-04-04 15:45:05 -0300

    fix(recipe): adjust recipe tests

--- __tests__/integration/recipe.test.js
+++ __tests__/integration/recipe.test.js
@@ -3,11 +3,6 @@ import MockAdapter from 'axios-mock-adapter';
 import axios from 'axios';
 import app from '../../src/app';
 
-import {
-  normalizeRecipeTitle,
-  mapRecipe
-} from '../../src/app/recipe/service/RecipeService';
-
 const endpoint = '/recipes';
 
 it('should return statusCode 200', async () => {
@@ -46,25 +41,4 @@ it('should throw recipesPuppy service is unnavailable', async () => {
 
   const response = await supertest(app).get(`${endpoint}?i=tomato`).send();
   expect(response.status).toBe(500);
-});
-
-it('should return only the title', () => {
-  const normalizedString = normalizeRecipeTitle('GWgheowig \r');
-  expect(normalizedString).toBe('GWgheowig');
-});
-
-it('should map the recipe body', () => {
-  const recipe = {
-    title:
-      'Roasted Garlic Grilling Sauce \r\n\t\t\r\n\t\r\n\t\t\r\n\t\r\n\t\t\r\n\t\r\n\t\r\n\r\n',
-    href:
-      'http://www.kraftfoods.com/kf/recipes/roasted-garlic-grilling-sauce-56344.aspx',
-    ingredients: 'garlic, onions, hot sauce',
-    thumbnail: 'http://img.recipepuppy.com/634118.jpg'
-  };
-  const mappedRecipe = mapRecipe(recipe);
-  expect(mappedRecipe.link).toBe(
-    'http://www.kraftfoods.com/kf/recipes/roasted-garlic-grilling-sauce-56344.aspx'
-  );
-  expect(mappedRecipe.title).toBe('Roasted Garlic Grilling Sauce');
 });
